9532d258364cc62232e3639df59b3be0f6d02ab4
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-02-24 17:00:26 -0300

    exercicio 6

---
+++ bloco_4/dia_1/6_exercicio.js
@@ -0,0 +1,33 @@
+//Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+let peca = "rainha"
+
+switch (peca) {
+    case "rei":
+        console.log("movimento uma casa em qualquer direção");
+        break;
+
+    case "rainha":
+        console.log("Movimenta em qualquer direção.");
+        break;
+
+    case "bispo":
+        console.log("Movimenta na diagonal.");
+        break;
+
+    case "torre":
+        console.log("Movimenta em reta.");
+        break;
+
+    case "cavalo":
+        console.log("Movimenta em L.");
+        break;
+
+    case "pião":
+        console.log("Movimenta uma casa para frente.");
+        break;    
+
+    default:
+        console.log("Isso não é uma peça de xadrez");
+        break;
+}
