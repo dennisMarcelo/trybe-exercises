dc1bac0031e98e711e3c6a124315d732eee4fe9f
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-02-24 16:14:15 -0300

    exercicio 4

---
+++ bloco_4/dia_1/4_exercicio.js
@@ -0,0 +1,10 @@
+//Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let a = -10;
+
+
+if( a > 0){
+    console.log("Positive")
+}else{
+    console.log("Negative")
+}
