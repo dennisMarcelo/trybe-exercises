d77388ec35192acd6054d79d3ec2bb27b8ca3c6b
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-02-25 16:17:53 -0300

    correçao

--- bloco_4/dia_2/bonus_1.js
+++ bloco_4/dia_2/bonus_1.js
@@ -1,6 +1,6 @@
 //Ordene o array numbers em ordem crescente e imprima seus valores;
 
-let array = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 1; i < array.length; i++) {
     for (let j = 0; j < i; j++) {
