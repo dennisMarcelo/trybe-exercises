f8390872a0f875cc66af7b2e2b61340e9ac517bb
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-03-05 18:46:26 -0300

    1_exercicio

---
+++ bloco_5/dia_3/1_exercicio_script.js
@@ -0,0 +1,41 @@
+function createDaysOfTheWeek() {
+	const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+	const weekDaysList = document.querySelector('.week-days');
+
+	for (let index = 0; index < weekDays.length; index += 1) {
+		const days = weekDays[index];
+		const dayListItem = document.createElement('li');
+		dayListItem.innerHTML = days;
+
+		weekDaysList.appendChild(dayListItem);
+	};
+};
+
+function pushDaysOfMonth () {
+	const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+	let ulDays = document.getElementById("days");
+
+	dezDaysList.forEach((e) => {
+		let $li = document.createElement("li");
+		$li.className = "day";
+		
+		if (e === 24 || e === 25 || e === 31) {
+			$li.className += " holiday";	
+		} 
+
+		if( e === 4 || e === 11 || e === 18 || e === 25 ){
+			$li.className += " friday";	
+		}
+
+		$li.innerText = e;
+		ulDays.appendChild($li);
+
+		
+
+		console.log($li);
+	})
+}
+
+createDaysOfTheWeek( );
+// Escreva seu código abaixo.
+pushDaysOfMonth();
