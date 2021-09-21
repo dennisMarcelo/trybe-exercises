b30ecfd9a0bd516dc816a31d79a6a0f7c00df61d
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-21 11:59:14 -0300

    retirado app.linten do api para poder não da problemas no teste.

--- bloco_28(Autenticação e Upload de Arquivos)/dia_03(Testando APIs com testes de integração)/aula - testes/src/api/server.js
+++ bloco_28(Autenticação e Upload de Arquivos)/dia_03(Testando APIs com testes de integração)/aula - testes/src/api/server.js
@@ -1,23 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const port = process.env.PORT || 8080;
+const app = require('./api');
 
-const routes = require('./routes');
-const validateJWT = require('./auth/validateJWT');
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-const apiRoutes = express.Router();
-
-apiRoutes.get('/api/posts', validateJWT, routes.getPosts);
-apiRoutes.post('/api/users', routes.createUsers);
-apiRoutes.post('/api/login', routes.login);
-
-app.use(apiRoutes);
-
-app.listen(PORT, () => console.log(`Conectado na porta ${PORT}`));
-
-module.exports = app;
+app.listen(port);
+console.log('conectado na porta ' + port);
---
+++ bloco_28(Autenticação e Upload de Arquivos)/dia_03(Testando APIs com testes de integração)/aula - testes/tests/findUserById.test.js
@@ -0,0 +1,98 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer} = require('mongodb-memory-server');
+
+
+const server = require('../src/api/api');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const fakeId = '605de6ded1ff223100cd6aa1'
+const user = { username: 'blackPanther', password: 'wakand forevar'}
+
+describe('GET /api/users/:userId', () => {
+
+  describe('requisição sem toke', async () => {
+    let responseWithoutToken;
+    const DBServer = new MongoMemoryServer();
+    
+    before(async () => {
+      const URLMock = await DBServer.getUri();
+      const connectionMock = await MongoClient.connect(URLMock,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+      );
+  
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+  
+      responseWithoutToken = await chai.request(server)
+        .post('/api/users')
+        .send(user);
+      console.log(responseWithoutToken.body);
+    })
+      
+    afterEach( async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
+      
+    it('retorna código de status 400 - /Not Found/', async () => {
+        expect(responseWithoutToken).to.have.status(400);
+    });
+    
+    it('retorna mensagem de erro no body da response com o texto /Token não encontrado ou informado/', () => {
+      expect(responseWithoutToken.body.message).to.be.equal('Token não encontrado ou informado')
+    });
+  });
+
+  //-----------------------------------------------------------
+  //-----------------------------------------------------------
+  //-----------------------------------------------------------
+  //-----------------------------------------------------------
+  
+  describe('requisição com token invalido', () => {
+    let responseInvalidToken;
+
+    it('retorna Código de status 401 - /Unauthorized/', () => {
+      expect(responseInvalidToken).to.have.status(401);
+    })
+
+    it('retorna mensagem no body da response com o texto /Acesso negado/', () => {
+      expect(responseInvalidToken.body.message).to.be.equal('Acesso negado')
+    });
+  });
+
+  describe('requisição com suario autenticado corretamente', () => {
+    let responseOK;
+
+    it('Código de status 200 - OK.', async () => {
+      expect(responseOK).to.have.status(200);
+    });
+
+    it('retorna os dados da pessoa usuária em um objeto em um objeto no corpo ( body ) da resposta ( response )', async () => {
+      expect(responseOK.body.message).to.be.equal('')
+    });
+  });
+});
+
+
+
+/* 
+  contratos GET /api/users/:userId: 
+    requisição sem toke  
+      - retorna código de status 400 - 'Not Found'.
+      - retorna Mensagem de erro no body da response com o texto 'Token não encontrado ou informado'.
+
+    requisição com token invalido
+      - retorna Código de status 401 - 'Unauthorized' 
+      - retorna mensagem no body da response com o 'texto Acesso negado'.
+    
+    requisição com suario autenticado corretamente
+      - retorna os dados da pessoa usuária em um objeto em um objeto no corpo ( body ) da resposta ( response ).
+      - Código de status 200 - OK.
+*/
+
